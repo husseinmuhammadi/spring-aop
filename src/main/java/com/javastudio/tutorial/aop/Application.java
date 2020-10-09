13ac2cf9710ae29312539abe5b0f8473a6ac23c4
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-10-09 22:08:25 +0330

    Spring AOP

--- src/main/java/com/javastudio/tutorial/aop/Application.java
+++ src/main/java/com/javastudio/tutorial/aop/Application.java
@@ -1,11 +1,27 @@
 package com.javastudio.tutorial.aop;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        LOGGER.info("Starting Spring Boot Application ...");
+
+        // assume SLF4J is bound to logback in the current environment
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        // print logback's internal status
+        StatusPrinter.print(lc);
+
         SpringApplication.run(Application.class, args);
+        LOGGER.info("Spring Boot Application started!");
+
     }
 }
