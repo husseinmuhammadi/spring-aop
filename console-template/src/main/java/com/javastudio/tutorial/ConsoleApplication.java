bc82f82011840a75519eb22fd9064e42b87bdedc
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-09-20 21:39:21 +0200

    Console template

---
+++ console-template/src/main/java/com/javastudio/tutorial/ConsoleApplication.java
@@ -0,0 +1,12 @@
+package com.javastudio.tutorial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConsoleApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConsoleApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello world!");
+    }
+}
