64b188a801205309586f23941bf5d33c0ac3343d
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-09-20 21:39:44 +0200

    Spring template

---
+++ spring-template/src/main/java/com/javastudio/tutorial/SpringApplication.java
@@ -0,0 +1,12 @@
+package com.javastudio.tutorial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SpringApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Hello world!");
+    }
+}
