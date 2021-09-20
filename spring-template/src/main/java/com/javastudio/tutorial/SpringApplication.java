5d555e8b3fba7c65548c429c66bb86fec9a35269
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-09-20 22:30:26 +0200

    Spring template

--- spring-template/src/main/java/com/javastudio/tutorial/SpringApplication.java
+++ spring-template/src/main/java/com/javastudio/tutorial/SpringApplication.java
@@ -1,12 +1,17 @@
 package com.javastudio.tutorial;
 
+import com.javastudio.tutorial.bean.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringApplication {
     private static final Logger LOGGER = LoggerFactory.getLogger(SpringApplication.class);
 
     public static void main(String[] args) {
-        LOGGER.info("Hello world!");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Product product = (Product) context.getBean("product");
+        LOGGER.info("product: {}", product.getName());
     }
 }
