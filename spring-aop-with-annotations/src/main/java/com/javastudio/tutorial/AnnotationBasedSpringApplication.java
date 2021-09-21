7994267cfce15c6353bf781119a5eb59d21bcd6c
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-09-21 13:08:54 +0200

    Spring AOP with annotations

---
+++ spring-aop-with-annotations/src/main/java/com/javastudio/tutorial/AnnotationBasedSpringApplication.java
@@ -0,0 +1,15 @@
+package com.javastudio.tutorial;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBasedSpringApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AnnotationBasedSpringApplication.class);
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        LOGGER.info("Spring context started!");
+    }
+}
