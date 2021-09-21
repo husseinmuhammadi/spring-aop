dcc6d2d651b6fb0221447eb0a16257d6c9b9c7d1
Hossein Mohammadi <hossein.mohammadi@specific-group.com>
2021-09-21 07:14:40 +0200

    Spring Context

--- spring-context-xml-configuration/src/main/java/com/javastudio/tutorial/SpringApplication.java
+++ spring-context-xml-configuration/src/main/java/com/javastudio/tutorial/SpringApplication.java
@@ -1,7 +1,18 @@
 package com.javastudio.tutorial;
 
-public class SpringApplication {
-    public static void main(String[] args) {
+import com.javastudio.tutorial.bean.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class SpringApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringApplication.class);
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Product product = (Product) context.getBean("product");
+        LOGGER.info("product: {}", product.getName());
     }
 }
